38b6bbdcb0564f3317bd0cc0cbabecea7685d830
lihao <1500465194@qq.com>
2021-06-12 10:09:13 +0800

    增加任务循环时间
    task_location为4ms轮询执行

--- application/task_location.c
+++ application/task_location.c
@@ -1,11 +1,14 @@
 #include "main.h"
 #include "mpu6500.h"
 #include "task_location.h"
+#include "cmsis_os.h"
 real_position_t real_position;//get the yaw (x ,y) position
 extern imu_t imu;
 void task_location(void *argument)
 {
 	float vx,vy;
+	while(1)
+	{
 	real_position.alpha = imu.yaw ; //get xoy yaw
 	//获取xoy平面的x，y速度
 	vx = imu.vx;
@@ -13,5 +16,7 @@ void task_location(void *argument)
 	//执行换算,执行时间为10ms
 	real_position.x = vx*10;
 	real_position.y = vy*10;
+	}
+	 osDelay(4);
 
 }
