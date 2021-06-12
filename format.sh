c896b1ed9b04e1bb24f0fa18dd9516d911a044e9
lihao <1500465194@qq.com>
2021-06-12 16:49:53 +0800

    更新 .clang-format

--- format.sh
+++ format.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-value_dir='application bsp component'
+value_dir='application bsp'
 
 find $value_dir -regex '.*\.\(cpp\|hpp\|cu\|c\|h\)' -exec clang-format --verbose -style=file -i {} \;
