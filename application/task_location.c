b98b4776202f1a12886be1d347fdc1236f5e8645
lihao <1500465194@qq.com>
2021-06-12 08:59:23 +0800

    增加文件
    增加了全场定位文件test_location

---
+++ application/task_location.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "mpu6500.h"
+#include "task_location.h"
+real_position_t real_position;//get the yaw (x ,y) position
+extern imu_t imu;
+void task_location(void *argument)
+{
+	float vx,vy;
+	real_position.alpha = imu.yaw ; //get xoy yaw
+	//获取xoy平面的x，y速度
+	vx = imu.vx;
+	vy = imu.vy;
+	//执行换算,执行时间为10ms
+	real_position.x = vx*10;
+	real_position.y = vy*10;
+
+}
---
+++ application/task_location.h
@@ -0,0 +1,14 @@
+#ifndef __TASK_LOCATION_H
+#define __TASK_LOCATION_H
+
+//set real position
+typedef struct
+	{
+	float x;
+	float y;
+	float alpha;
+}real_position_t;
+
+
+#endif
+
