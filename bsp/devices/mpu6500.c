516c7d65340aff1098276acb32e98aae320f5df8
lihao <1500465194@qq.com>
2021-07-04 08:16:13 +0800

    perf(MPU_NSS_*): 封装成宏函数

--- bsp/devices/mpu6500.c
+++ bsp/devices/mpu6500.c
@@ -32,8 +32,8 @@ extern SPI_HandleTypeDef hspi;
 #define MPU_DELAY delay_ms
 #endif /* USED_OS */
 
-#define MPU_NSS_LOW  MPU_NSS_PORT->BSRR = (uint32_t)MPU_NSS_PIN << 16U
-#define MPU_NSS_HIGH MPU_NSS_PORT->BSRR = MPU_NSS_PIN
+#define MPU_NSS_LOW()  (MPU_NSS_PORT->BSRR = (uint32_t)MPU_NSS_PIN << 16)
+#define MPU_NSS_HIGH() (MPU_NSS_PORT->BSRR = MPU_NSS_PIN)
 
 static uint8_t buff_mpu[14]; /*!< buffer to save MPU6500 raw data */
 static uint8_t buff_ist[6];  /*!< buffer to save IST8310 raw data */
@@ -50,15 +50,15 @@ imu_t imu;
 */
 static void mpu_write_byte(uint8_t const reg, uint8_t const data)
 {
-    MPU_NSS_LOW;
+    MPU_NSS_LOW();
 
-    tx = reg & 0x7FU;
-    (void)HAL_SPI_TransmitReceive(&hspi, &tx, &rx, 1U, 55U);
+    tx = reg & 0x7F;
+    (void)HAL_SPI_TransmitReceive(&hspi, &tx, &rx, 1, 55);
 
     tx = data;
-    (void)HAL_SPI_TransmitReceive(&hspi, &tx, &rx, 1U, 55U);
+    (void)HAL_SPI_TransmitReceive(&hspi, &tx, &rx, 1, 55);
 
-    MPU_NSS_HIGH;
+    MPU_NSS_HIGH();
 }
 
 /**
@@ -68,14 +68,14 @@ static void mpu_write_byte(uint8_t const reg, uint8_t const data)
 */
 static uint8_t mpu_read_byte(uint8_t const reg)
 {
-    MPU_NSS_LOW;
+    MPU_NSS_LOW();
 
-    tx = reg | 0x80U;
+    tx = reg | 0x80;
 
-    (void)HAL_SPI_TransmitReceive(&hspi, &tx, &rx, 1U, 55U);
-    (void)HAL_SPI_TransmitReceive(&hspi, &tx, &rx, 1U, 55U);
+    (void)HAL_SPI_TransmitReceive(&hspi, &tx, &rx, 1, 55);
+    (void)HAL_SPI_TransmitReceive(&hspi, &tx, &rx, 1, 55);
 
-    MPU_NSS_HIGH;
+    MPU_NSS_HIGH();
 
     return rx;
 }
@@ -88,14 +88,14 @@ static uint8_t mpu_read_byte(uint8_t const reg)
 */
 static void mpu_read_bytes(uint8_t const reg, void *buf, uint8_t len)
 {
-    MPU_NSS_LOW;
+    MPU_NSS_LOW();
 
-    tx = reg | 0x80U;
+    tx = reg | 0x80;
 
-    (void)HAL_SPI_TransmitReceive(&hspi, &tx, &rx, 1U, 55U);
-    (void)HAL_SPI_TransmitReceive(&hspi, &tx, buf, len, 55U);
+    (void)HAL_SPI_TransmitReceive(&hspi, &tx, &rx, 1, 55);
+    (void)HAL_SPI_TransmitReceive(&hspi, &tx, buf, len, 55);
 
-    MPU_NSS_HIGH;
+    MPU_NSS_HIGH();
 }
 
 /**
@@ -106,20 +106,20 @@ static void mpu_read_bytes(uint8_t const reg, void *buf, uint8_t len)
 static void ist_write_byte(uint8_t addr, uint8_t data)
 {
     /*!< turn off slave 1 at first */
-    mpu_write_byte(MPU6500_I2C_SLV1_CTRL, 0x00U);
-    MPU_DELAY(2U);
+    mpu_write_byte(MPU6500_I2C_SLV1_CTRL, 0x00);
+    MPU_DELAY(2);
 
     mpu_write_byte(MPU6500_I2C_SLV1_REG, addr);
-    MPU_DELAY(2U);
+    MPU_DELAY(2);
 
     mpu_write_byte(MPU6500_I2C_SLV1_DO, data);
-    MPU_DELAY(2U);
+    MPU_DELAY(2);
 
     /*!< turn on slave 1 with one byte transmitting */
-    mpu_write_byte(MPU6500_I2C_SLV1_CTRL, 0x80U | 0x01U);
+    mpu_write_byte(MPU6500_I2C_SLV1_CTRL, 0x80 | 0x01);
 
     /*!< wait longer to ensure the data is transmitted from slave 1 */
-    MPU_DELAY(10U);
+    MPU_DELAY(10);
 }
 
 /**
@@ -132,16 +132,16 @@ static uint8_t ist_read_byte(uint8_t addr)
     uint8_t ret;
 
     mpu_write_byte(MPU6500_I2C_SLV4_REG, addr);
-    MPU_DELAY(10U);
+    MPU_DELAY(10);
 
-    mpu_write_byte(MPU6500_I2C_SLV4_CTRL, 0x80U);
-    MPU_DELAY(10U);
+    mpu_write_byte(MPU6500_I2C_SLV4_CTRL, 0x80);
+    MPU_DELAY(10);
 
     ret = mpu_read_byte(MPU6500_I2C_SLV4_DI);
 
     /*!< turn off slave4 after read */
-    mpu_write_byte(MPU6500_I2C_SLV4_CTRL, 0x00U);
-    MPU_DELAY(10U);
+    mpu_write_byte(MPU6500_I2C_SLV4_CTRL, 0x00);
+    MPU_DELAY(10);
 
     return ret;
 }
@@ -159,38 +159,38 @@ static void mpu_i2c_config(uint8_t id, uint8_t reg, uint8_t len)
      * use slave1, auto transmit single measure mode
     */
     mpu_write_byte(MPU6500_I2C_SLV1_ADDR, id);
-    MPU_DELAY(2U);
+    MPU_DELAY(2);
 
     mpu_write_byte(MPU6500_I2C_SLV1_REG, IST8310_R_CONFA);
-    MPU_DELAY(2U);
+    MPU_DELAY(2);
 
     mpu_write_byte(MPU6500_I2C_SLV1_DO, IST8310_ODR_MODE);
-    MPU_DELAY(2U);
+    MPU_DELAY(2);
 
     /*!< use slave0,auto read data */
-    mpu_write_byte(MPU6500_I2C_SLV0_ADDR, 0x80U | id);
-    MPU_DELAY(2U);
+    mpu_write_byte(MPU6500_I2C_SLV0_ADDR, 0x80 | id);
+    MPU_DELAY(2);
 
     mpu_write_byte(MPU6500_I2C_SLV0_REG, reg);
-    MPU_DELAY(2U);
+    MPU_DELAY(2);
 
     /*!< every eight mpu6500 internal samples one i2c master read */
-    mpu_write_byte(MPU6500_I2C_SLV4_CTRL, 0x03U);
-    MPU_DELAY(2U);
+    mpu_write_byte(MPU6500_I2C_SLV4_CTRL, 0x03);
+    MPU_DELAY(2);
 
     /*!< enable slave 0 and 1 access delay */
-    mpu_write_byte(MPU6500_I2C_MST_DELAY_CTRL, 0x01U | 0x02U);
-    MPU_DELAY(2U);
+    mpu_write_byte(MPU6500_I2C_MST_DELAY_CTRL, 0x01 | 0x02);
+    MPU_DELAY(2);
 
     /*!< enable slave 1 auto transmit */
-    mpu_write_byte(MPU6500_I2C_SLV1_CTRL, 0x80U | 0x01U);
+    mpu_write_byte(MPU6500_I2C_SLV1_CTRL, 0x80 | 0x01);
 
     /*!< Wait 6ms (minimum waiting time for 16 times internal average setup) */
-    MPU_DELAY(6U);
+    MPU_DELAY(6);
 
     /*!< enable slave 0 with len bytes reading */
-    mpu_write_byte(MPU6500_I2C_SLV0_CTRL, 0x80U | len);
-    MPU_DELAY(2U);
+    mpu_write_byte(MPU6500_I2C_SLV0_CTRL, 0x80 | len);
+    MPU_DELAY(2);
 }
 
 /**
@@ -199,77 +199,77 @@ static void mpu_i2c_config(uint8_t id, uint8_t reg, uint8_t len)
 uint8_t ist_init(void)
 {
     /*!< Enable iic master mode */
-    mpu_write_byte(MPU6500_USER_CTRL, 0x30U);
-    MPU_DELAY(10U);
+    mpu_write_byte(MPU6500_USER_CTRL, 0x30);
+    MPU_DELAY(10);
 
     /*!< Enable iic 400khz */
-    mpu_write_byte(MPU6500_I2C_MST_CTRL, 0x0DU);
-    MPU_DELAY(10U);
+    mpu_write_byte(MPU6500_I2C_MST_CTRL, 0x0D);
+    MPU_DELAY(10);
 
     /*!< Turn on slave 1 for ist write and slave 4 to ist read */
     mpu_write_byte(MPU6500_I2C_SLV1_ADDR, IST8310_ADDRESS);
-    MPU_DELAY(10U);
+    MPU_DELAY(10);
 
-    mpu_write_byte(MPU6500_I2C_SLV4_ADDR, 0x80U | IST8310_ADDRESS);
-    MPU_DELAY(10U);
+    mpu_write_byte(MPU6500_I2C_SLV4_ADDR, 0x80 | IST8310_ADDRESS);
+    MPU_DELAY(10);
 
     /* IST8310_R_CONFB 0x01 = device rst */
-    ist_write_byte(IST8310_R_CONFB, 0x01U);
-    MPU_DELAY(10U);
+    ist_write_byte(IST8310_R_CONFB, 0x01);
+    MPU_DELAY(10);
 
     if (IST8310_DEVICE_ID_A != ist_read_byte(IST8310_WHO_AM_I))
     {
-        return 1U;
+        return 1;
     }
 
     /* Soft reset */
-    ist_write_byte(IST8310_R_CONFB, 0x01U);
-    MPU_DELAY(10U);
+    ist_write_byte(IST8310_R_CONFB, 0x01);
+    MPU_DELAY(10);
 
     /* Config as ready mode to access register */
-    ist_write_byte(IST8310_R_CONFA, 0x00U);
-    if (ist_read_byte(IST8310_R_CONFA) != 0x00U)
+    ist_write_byte(IST8310_R_CONFA, 0x00);
+    if (ist_read_byte(IST8310_R_CONFA) != 0x00)
     {
-        return 2U;
+        return 2;
     }
-    MPU_DELAY(10U);
+    MPU_DELAY(10);
 
     /* Normal state, no int */
-    ist_write_byte(IST8310_R_CONFB, 0x00U);
-    if (ist_read_byte(IST8310_R_CONFB) != 0x00U)
+    ist_write_byte(IST8310_R_CONFB, 0x00);
+    if (ist_read_byte(IST8310_R_CONFB) != 0x00)
     {
-        return 3U;
+        return 3;
     }
-    MPU_DELAY(10U);
+    MPU_DELAY(10);
 
     /* Config low noise mode, x,y,z axis 16 time 1 avg */
-    ist_write_byte(IST8310_AVGCNTL, 0x24U); /* 100100 */
-    if (ist_read_byte(IST8310_AVGCNTL) != 0x24U)
+    ist_write_byte(IST8310_AVGCNTL, 0x24); /* 100100 */
+    if (ist_read_byte(IST8310_AVGCNTL) != 0x24)
     {
-        return 4U;
+        return 4;
     }
-    MPU_DELAY(10U);
+    MPU_DELAY(10);
 
     /* Set/Reset pulse duration setup,normal mode */
-    ist_write_byte(IST8310_PDCNTL, 0xC0U);
-    if (ist_read_byte(IST8310_PDCNTL) != 0xC0U)
+    ist_write_byte(IST8310_PDCNTL, 0xC0);
+    if (ist_read_byte(IST8310_PDCNTL) != 0xC0)
     {
-        return 5U;
+        return 5;
     }
-    MPU_DELAY(10U);
+    MPU_DELAY(10);
 
     /* Turn off slave1 & slave 4 */
-    mpu_write_byte(MPU6500_I2C_SLV1_CTRL, 0x00U);
-    MPU_DELAY(10U);
+    mpu_write_byte(MPU6500_I2C_SLV1_CTRL, 0x00);
+    MPU_DELAY(10);
 
-    mpu_write_byte(MPU6500_I2C_SLV4_CTRL, 0x00U);
-    MPU_DELAY(10U);
+    mpu_write_byte(MPU6500_I2C_SLV4_CTRL, 0x00);
+    MPU_DELAY(10);
 
     /* Configure and turn on slave 0 */
-    mpu_i2c_config(IST8310_ADDRESS, IST8310_R_XL, 0x06U);
-    MPU_DELAY(100U);
+    mpu_i2c_config(IST8310_ADDRESS, IST8310_R_XL, 0x06);
+    MPU_DELAY(100);
 
-    return 0U;
+    return 0;
 }
 
 /**
@@ -278,7 +278,7 @@ uint8_t ist_init(void)
 */
 void ist_data_update(void *buff)
 {
-    mpu_read_bytes(MPU6500_EXT_SENS_DATA_00, buff, 6U);
+    mpu_read_bytes(MPU6500_EXT_SENS_DATA_00, buff, 6);
 }
 
 /**
@@ -386,14 +386,14 @@ void mpu_device_init(void)
         {MPU6500_USER_CTRL, 0x20},
     }; /* Enable AUX */
 
-    for (uint8_t i = 0U; i != 10U; ++i)
+    for (uint8_t i = 0; i != 10; ++i)
     {
         mpu_write_byte(data[i][0], data[i][1]);
-        MPU_DELAY(1U);
+        MPU_DELAY(1);
     }
 
-    mpu_set_gyro_fsr(3U);
-    mpu_set_accel_fsr(2U);
+    mpu_set_gyro_fsr(3);
+    mpu_set_accel_fsr(2);
 
     if (ist_init())
     {
