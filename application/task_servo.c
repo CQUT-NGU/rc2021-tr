ef0c234ab61006e0ccd79353f5362a4760e70f1e
lihao <1500465194@qq.com>
2021-07-10 22:40:25 +0800

    feat: 添加舵机驱动

--- application/task_servo.c
+++ application/task_servo.c
@@ -19,76 +19,75 @@
 #include "cmsis_os.h"
 #endif /* USED_OS */
 
-extern TIM_HandleTypeDef htim4;
+#define MIDDLE_TIM htim2
+#define BESIDE_TIM htim4
+
+extern TIM_HandleTypeDef MIDDLE_TIM;
+extern TIM_HandleTypeDef BESIDE_TIM;
 
 #define SERVO_PSC     90
 #define SERVO_PWM_MAX 20000
 #define SERVO_PWM_MID 1500
 
-#define SERVO_TIM      htim4
-#define GETL_CHANNEL   TIM_CHANNEL_1
-#define GETR_CHANNEL   TIM_CHANNEL_2
-#define SHIFTV_CHANNEL TIM_CHANNEL_3
-#define PITCH_CHANNEL  TIM_CHANNEL_4
+#define FETCH_CHANNEL  TIM_CHANNEL_2
+#define PITCH_CHANNEL  TIM_CHANNEL_3
+#define SHIFTV_CHANNEL TIM_CHANNEL_4
+
+#define PITCH_L_CHANNEL  TIM_CHANNEL_1
+#define SHIFTV_L_CHANNEL TIM_CHANNEL_2
+#define PITCH_R_CHANNEL  TIM_CHANNEL_3
+#define SHIFTV_R_CHANNEL TIM_CHANNEL_4
 
 #define FLAG_RUN_GETL   (1 << 0)
 #define FLAG_RUN_GETR   (1 << 1)
 #define FLAG_RUN_SHIFTV (1 << 2)
 #define FLAG_RUN_PITCH  (1 << 3)
 
-#define PWM_INIT_GET 0
-#define PWM_LIFT_GET 100
-#define PWM_LAST_GET 750
-
 #define PWM_INIT_PITCH 1275
 
 #define PWM_INIT_SHIFTV 1000
 #define PWM_LAST_SHIFTV 2300
 
-#define SHOOT_COUNT      50
-#define FLAG_SHOOT_START (1 << 0)
-#define FLAG_SHOOT_COUNT (1 << 1)
-#define FLAG_SHOOT_STOP  (1 << 2)
-
-static void servo_init(void)
+__STATIC_INLINE
+void fetch_set(uint16_t pwm)
 {
-    __HAL_TIM_SET_PRESCALER(&SERVO_TIM, SERVO_PSC - 1);
-    __HAL_TIM_SetAutoreload(&SERVO_TIM, SERVO_PWM_MAX - 1);
-    HAL_TIM_Base_Start(&SERVO_TIM);
+    __HAL_TIM_SET_COMPARE(&MIDDLE_TIM, FETCH_CHANNEL, pwm);
 }
 
-static void servo_start(void)
+__STATIC_INLINE
+void pitch_set(uint16_t pwm)
 {
-    HAL_TIM_PWM_Start(&SERVO_TIM, GETL_CHANNEL);
-    HAL_TIM_PWM_Start(&SERVO_TIM, GETR_CHANNEL);
-    HAL_TIM_PWM_Start(&SERVO_TIM, SHIFTV_CHANNEL);
-    HAL_TIM_PWM_Start(&SERVO_TIM, PITCH_CHANNEL);
+    __HAL_TIM_SET_COMPARE(&MIDDLE_TIM, PITCH_CHANNEL, pwm);
 }
 
-static inline void getl_set(uint16_t pwm)
+__STATIC_INLINE
+void pitchl_set(uint16_t pwm)
 {
-    __HAL_TIM_SetCompare(&SERVO_TIM, GETL_CHANNEL, pwm);
+    __HAL_TIM_SET_COMPARE(&BESIDE_TIM, PITCH_L_CHANNEL, pwm);
 }
 
-static inline void getr_set(uint16_t pwm)
+__STATIC_INLINE
+void pitchr_set(uint16_t pwm)
 {
-    __HAL_TIM_SetCompare(&SERVO_TIM, GETL_CHANNEL, pwm);
+    __HAL_TIM_SET_COMPARE(&BESIDE_TIM, PITCH_R_CHANNEL, pwm);
 }
 
-static inline void get_set(int16_t x)
+__STATIC_INLINE
+void shiftv_set(uint16_t pwm)
 {
-    __HAL_TIM_SetCompare(&SERVO_TIM, GETL_CHANNEL, (uint32_t)(1500 - x));
-    __HAL_TIM_SetCompare(&SERVO_TIM, GETR_CHANNEL, (uint32_t)(1500 + x));
+    __HAL_TIM_SET_COMPARE(&MIDDLE_TIM, SHIFTV_CHANNEL, pwm);
 }
 
-static inline void shiftv_set(uint16_t pwm)
+__STATIC_INLINE
+void shiftvl_set(uint16_t pwm)
 {
-    __HAL_TIM_SetCompare(&SERVO_TIM, SHIFTV_CHANNEL, pwm);
+    __HAL_TIM_SET_COMPARE(&BESIDE_TIM, SHIFTV_L_CHANNEL, pwm);
 }
 
-static inline void pitch_set(uint16_t pwm)
+__STATIC_INLINE
+void shiftvr_set(uint16_t pwm)
 {
-    __HAL_TIM_SetCompare(&SERVO_TIM, PITCH_CHANNEL, pwm);
+    __HAL_TIM_SET_COMPARE(&MIDDLE_TIM, SHIFTV_R_CHANNEL, pwm);
 }
 
 void task_servo(void *pvParameters)
@@ -99,21 +98,35 @@ void task_servo(void *pvParameters)
 
     ctrl_pc_t *pc = ctrl_pc_point();
 
-    servo_init();
-    servo_start();
+    {
+        __HAL_TIM_SET_PRESCALER(&MIDDLE_TIM, SERVO_PSC - 1);
+        __HAL_TIM_SetAutoreload(&MIDDLE_TIM, SERVO_PWM_MAX - 1);
+        HAL_TIM_Base_Start(&MIDDLE_TIM);
 
-    get_set(PWM_INIT_GET);
-    pitch_set(PWM_INIT_PITCH);
-    shiftv_set(PWM_INIT_SHIFTV);
+        __HAL_TIM_SET_PRESCALER(&BESIDE_TIM, SERVO_PSC - 1);
+        __HAL_TIM_SetAutoreload(&BESIDE_TIM, SERVO_PWM_MAX - 1);
+        HAL_TIM_Base_Start(&BESIDE_TIM);
+    }
+
+    {
+        HAL_TIM_PWM_Start(&MIDDLE_TIM, FETCH_CHANNEL);
+        HAL_TIM_PWM_Start(&MIDDLE_TIM, SHIFTV_CHANNEL);
+        HAL_TIM_PWM_Start(&MIDDLE_TIM, PITCH_CHANNEL);
+
+        HAL_TIM_PWM_Start(&BESIDE_TIM, PITCH_L_CHANNEL);
+        HAL_TIM_PWM_Start(&BESIDE_TIM, SHIFTV_L_CHANNEL);
+        HAL_TIM_PWM_Start(&BESIDE_TIM, PITCH_R_CHANNEL);
+        HAL_TIM_PWM_Start(&BESIDE_TIM, SHIFTV_R_CHANNEL);
+    }
+
+    // get_set(PWM_INIT_GET);
+    // pitch_set(PWM_INIT_PITCH);
+    // shiftv_set(PWM_INIT_SHIFTV);
 
     osDelay(500);
 
-    int16_t pwm_get_set = 0;
-
-    int16_t pwm_get = 0;
-
-    int8_t shoot_flag = 0;
-    uint8_t shoot_count = 0;
+    uint16_t pwm_fetch_set = 1500;
+    uint16_t pwm_fetch = 0;
 
     while (1)
     {
@@ -121,7 +134,7 @@ void task_servo(void *pvParameters)
         {
         case 'a':
         {
-            pwm_get_set = (int16_t)pc->x;
+            pwm_fetch_set = (uint16_t)pc->x;
             pitch_set((uint16_t)pc->y);
             shiftv_set((uint16_t)pc->z);
 
@@ -151,7 +164,7 @@ void task_servo(void *pvParameters)
                 */
                 else if (rc->rc.ch[RC_CH_S] < -110)
                 {
-                    shiftv_set(PWM_LAST_SHIFTV);
+                    //shiftv_set(PWM_LAST_SHIFTV);
                 }
 
                 /* The steering gear moves backwards vertically */
@@ -169,7 +182,6 @@ void task_servo(void *pvParameters)
                 /* Lift the arrow horizontally */
                 else if (rc->rc.ch[RC_CH_RV] < -550)
                 {
-                    pwm_get_set = PWM_LAST_GET;
                 }
                 /* -440 ~ -10, Clip the arrow and lift it */
                 else if (rc->rc.ch[RC_CH_RV] < -110)
@@ -177,43 +189,6 @@ void task_servo(void *pvParameters)
                     /* Clamp the arrow */
                     gpio_pin_set(POWER3_RU_GPIO_Port, POWER3_RU_Pin);
                     /* Lift it */
-                    if (pwm_get_set == PWM_INIT_GET)
-                    {
-                        pwm_get_set = PWM_LIFT_GET;
-                    }
-                }
-            }
-            /* Pull lever up to maximum */
-            else if (rc->rc.ch[RC_CH_LV] > 650)
-            {
-                /* Return to initial position */
-                pwm_get_set = PWM_INIT_GET;
-            }
-            /* The tie rod is in the normal position */
-            else
-            {
-                /* relay control, shoot an arrow */
-                if (rc->rc.ch[RC_CH_S] < -650)
-                {
-                    /* It starts to spew out gas */
-                    if (!READ_BIT(shoot_flag, FLAG_SHOOT_START))
-                    {
-                        SET_BIT(shoot_flag, FLAG_SHOOT_START);
-                        SET_BIT(shoot_flag, FLAG_SHOOT_COUNT);
-
-                        gpio_pin_set(POWER1_LU_GPIO_Port, POWER1_LU_Pin);
-                    }
-                }
-                else
-                {
-                    /* Stop ejecting gas */
-                    if (READ_BIT(shoot_flag, FLAG_SHOOT_STOP))
-                    {
-                        CLEAR_BIT(shoot_flag, FLAG_SHOOT_STOP);
-                        CLEAR_BIT(shoot_flag, FLAG_SHOOT_START);
-                    }
-
-                    gpio_pin_reset(POWER1_LU_GPIO_Port, POWER1_LU_Pin);
                 }
             }
         }
@@ -223,35 +198,22 @@ void task_servo(void *pvParameters)
          * prevent the PWM from changing too much to
          * cause the steering gear to jam
         */
-        int16_t delta = (int16_t)(pwm_get - pwm_get_set);
+        int32_t delta = pwm_fetch - pwm_fetch_set;
         if (delta < 0)
         {
-            pwm_get++;
+            pwm_fetch++;
         }
         else if (delta > 0)
         {
-            pwm_get--;
+            pwm_fetch--;
         }
         if (delta)
         {
-            get_set(pwm_get);
-        }
-
-        /* Control injection time */
-        if (READ_BIT(shoot_flag, FLAG_SHOOT_COUNT))
-        {
-            if (shoot_count++ == SHOOT_COUNT)
-            {
-                shoot_count = 0;
-                CLEAR_BIT(shoot_flag, FLAG_SHOOT_COUNT);
-
-                gpio_pin_reset(POWER1_LU_GPIO_Port, POWER1_LU_Pin);
-                SET_BIT(shoot_flag, FLAG_SHOOT_STOP);
-            }
+            __HAL_TIM_SET_COMPARE(&MIDDLE_TIM, FETCH_CHANNEL, pwm_fetch);
         }
 
         /* Task delay */
-        osDelay(4);
+        osDelay(2);
     }
 }
 
